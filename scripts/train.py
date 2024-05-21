d391d85c583a228741469fe2a6cd58358a75e2b1
Neo Yin <yinzhang@berkeley.edu>
2024-05-21 11:12:08 -0700

    update to resnext50

--- scripts/train.py
+++ scripts/train.py
@@ -12,21 +12,22 @@ from torch import nn
 from pytorch_lightning.loggers import TensorBoardLogger
 from torchvision import transforms, datasets, models
 from torchmetrics import Accuracy, AUROC
+from torch.utils.data import WeightedRandomSampler
 
 
 ############################################################################
 ####### DEFINE HYPERPARAMETERS AND DATA DIRECTORIES ########################
 ############################################################################
 
+num_epochs = 500
 default_config = {"lr": 3.56e-07}  # 1.462801279401232e-06}
-num_epochs = 500  # 200
-data_dir = "/media/hdd1/neo/bma_region_clf_data_full_v2_split"
+data_dir = "/media/hdd1/neo/pooled_deepheme_data"
 num_gpus = 3
 num_workers = 20
 downsample_factor = 1
-batch_size = 32
-num_classes = 2
+batch_size = 256
 img_size = 96
+num_classes = 23
 
 ############################################################################
 ####### FUNCTIONS FOR DATA AUGMENTATION AND DATA LOADING ###################
@@ -81,7 +82,7 @@ class DownsampledDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         image, label = self.dataset[idx]
         if self.downsample_factor > 1:
-            size = (img_size // self.downsample_factor, img_size // self.downsample_factor)
+            size = (96 // self.downsample_factor, 96 // self.downsample_factor)
             image = transforms.functional.resize(image, size)
 
         # Convert image to RGB if not already
@@ -92,7 +93,7 @@ class DownsampledDataset(torch.utils.data.Dataset):
         if self.apply_augmentation:
             # Apply augmentation
             image = get_feat_extract_augmentation_pipeline(
-                image_size=img_size // self.downsample_factor
+                image_size=96 // self.downsample_factor
             )(image=np.array(image))["image"]
 
         image = to_tensor(image)
@@ -100,7 +101,6 @@ class DownsampledDataset(torch.utils.data.Dataset):
         return image, label
 
 
-# Data Module
 class ImageDataModule(pl.LightningDataModule):
     def __init__(self, data_dir, batch_size, downsample_factor):
         super().__init__()
@@ -110,6 +110,7 @@ class ImageDataModule(pl.LightningDataModule):
         self.transform = transforms.Compose(
             [
                 transforms.ToTensor(),
+                # Additional normalization can be uncommented and adjusted if needed
                 # transforms.Normalize(mean=(0.61070228, 0.54225375, 0.65411311), std=(0.1485182, 0.1786308, 0.12817113))
             ]
         )
@@ -117,18 +118,16 @@ class ImageDataModule(pl.LightningDataModule):
     def setup(self, stage=None):
         # Load train, validation and test datasets
         train_dataset = datasets.ImageFolder(
-            root=os.path.join(self.data_dir, "train"),
-            transform=self.transform,
+            root=os.path.join(self.data_dir, "train"), transform=self.transform
         )
         val_dataset = datasets.ImageFolder(
-            root=os.path.join(self.data_dir, "val"),
-            transform=self.transform,
+            root=os.path.join(self.data_dir, "val"), transform=self.transform
         )
         test_dataset = datasets.ImageFolder(
-            root=os.path.join(self.data_dir, "test"),
-            transform=self.transform,
+            root=os.path.join(self.data_dir, "test"), transform=self.transform
         )
 
+        # Prepare the train dataset with downsampling and augmentation
         self.train_dataset = DownsampledDataset(
             train_dataset, self.downsample_factor, apply_augmentation=True
         )
@@ -139,56 +138,64 @@ class ImageDataModule(pl.LightningDataModule):
             test_dataset, self.downsample_factor, apply_augmentation=False
         )
 
+        # Compute class weights for handling imbalance
+        class_counts = torch.tensor([t[1] for t in train_dataset.samples]).bincount()
+        class_weights = 1.0 / class_counts.float()
+        sample_weights = class_weights[[t[1] for t in train_dataset.samples]]
+
+        self.train_sampler = WeightedRandomSampler(
+            weights=sample_weights, num_samples=len(sample_weights), replacement=True
+        )
+
     def train_dataloader(self):
         return DataLoader(
             self.train_dataset,
             batch_size=self.batch_size,
-            shuffle=True,
-            num_workers=num_workers,
+            sampler=self.train_sampler,
+            num_workers=20,
         )
 
     def val_dataloader(self):
         return DataLoader(
-            self.val_dataset,
-            batch_size=self.batch_size,
-            shuffle=False,
-            num_workers=num_workers,
+            self.val_dataset, batch_size=self.batch_size, shuffle=False, num_workers=20
         )
 
     def test_dataloader(self):
         return DataLoader(
-            self.test_dataset,
-            batch_size=self.batch_size,
-            shuffle=False,
-            num_workers=num_workers,
+            self.test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=20
         )
 
 
 # Model Module
-class ResNetModel(pl.LightningModule):
-    def __init__(self, num_classes=2, config=default_config):
-        super().__init__()
-        self.model = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V1)
-        self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
-
-        assert num_classes >= 2
+class Myresnext50(pl.LightningModule):
+    def __init__(self, my_pretrained_model, num_classes=23, config=default_config):
+        super(Myresnext50, self).__init__()
+        self.pretrained = my_pretrained_model
+        self.my_new_layers = nn.Sequential(
+            nn.Linear(
+                1000, 100
+            ),  # Assuming the output of your pre-trained model is 1000
+            nn.ReLU(),
+            nn.Linear(100, num_classes),
+        )
+        self.num_classes = num_classes
 
         if num_classes == 2:
             task = "binary"
         elif num_classes > 2:
             task = "multiclass"
-
-        task = "multiclass"
-
-        self.train_accuracy = Accuracy(task=task, num_classes=num_classes)
-        self.val_accuracy = Accuracy(task=task, num_classes=num_classes)
+        self.train_accuracy = Accuracy(num_classes=num_classes, task=task)
+        self.val_accuracy = Accuracy(num_classes=num_classes, task=task)
         self.train_auroc = AUROC(num_classes=num_classes, task=task)
         self.val_auroc = AUROC(num_classes=num_classes, task=task)
 
         self.config = config
 
     def forward(self, x):
-        return self.model(x)
+        x = self.pretrained(x)
+        x = self.my_new_layers(x)
+
+        return x
 
     def training_step(self, batch, batch_idx):
         x, y = batch
@@ -202,12 +209,8 @@ class ResNetModel(pl.LightningModule):
         return loss
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.config["lr"])
-
-        # T_max is the number of steps until the first restart (here, set to total training epochs).
-        # eta_min is the minimum learning rate. Adjust these parameters as needed.
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.config["lr"])
         scheduler = CosineAnnealingLR(optimizer, T_max=num_epochs, eta_min=0)
-
         return {"optimizer": optimizer, "lr_scheduler": scheduler}
 
     def validation_step(self, batch, batch_idx):
@@ -222,20 +225,20 @@ class ResNetModel(pl.LightningModule):
     def on_validation_epoch_end(self):
         self.log("val_acc_epoch", self.val_accuracy.compute())
         self.log("val_auroc_epoch", self.val_auroc.compute())
-        # Handle or reset saved outputs as needed
-
         current_lr = self.trainer.optimizers[0].param_groups[0]["lr"]
         self.log("learning_rate", current_lr, on_epoch=True)
 
 
 # Main training loop
-def train_model(downsample_factor):
+def train_model(downsample_factor, my_pretrained_model):
     data_module = ImageDataModule(
         data_dir=data_dir,
         batch_size=batch_size,
         downsample_factor=downsample_factor,
     )
-    model = ResNetModel(num_classes=num_classes)
+    model = Myresnext50(
+        num_classes=num_classes, my_pretrained_model=my_pretrained_model
+    )
 
     # Logger
     logger = TensorBoardLogger("lightning_logs", name=str(downsample_factor))
@@ -252,4 +255,11 @@ def train_model(downsample_factor):
 
 if __name__ == "__main__":
     # Run training for each downsampling factor
-    train_model(downsample_factor=downsample_factor)
+
+    # Load the pretrained model
+    my_pretrained_model = models.resnext50_32x4d(pretrained=True)
+
+    # Train the model
+    train_model(
+        downsample_factor=downsample_factor, my_pretrained_model=my_pretrained_model
+    )
